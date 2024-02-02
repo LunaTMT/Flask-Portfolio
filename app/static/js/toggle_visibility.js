6626d55ce2ea234017dae28c8c52c401b774ca4b
Маршалл <44672093+LunaTMT@users.noreply.github.com>
2024-02-02 18:06:31 +0000

    Add files via upload

--- app/static/js/toggle_visibility.js
+++ app/static/js/toggle_visibility.js
@@ -1,11 +1,13 @@
-function toggleContainerVisibility(containerId) {
-    var containers = document.querySelectorAll('.description-container, .overview-container, .video-container, .devlog-container, .tutorial-container');
+function toggleContainerVisibility(containerId, parentContainerId=NaN) {
+  var containers = document.querySelectorAll('.description-container, .overview-container, .video-container, .devlog-container, .tutorial-container, .about-container, .skills-container, .PL-container, .interests-container, .languages-container');
 
-    containers.forEach(function(container) {
+  containers.forEach(function(container) {
       if (container.id === containerId) {
-        container.classList.toggle('hide');
+          container.classList.toggle('hide');
+      } else if (parentContainerId && container.id === parentContainerId) {
+          // Skip hiding the parent container
       } else {
-        container.classList.add('hide');
+          container.classList.add('hide');
       }
-    });
-  }
+  });
+}
